2c974c8db2fc8c0c4e785c1fdce32bfc7acee736
linnelisek <linnelisek@gmail.com>
2024-02-27 18:19:22 +0100

    Made door_state

---
+++ skeleton_project/source/door_state.c
@@ -0,0 +1,47 @@
+#include "hardware.h"
+#include "channels.h"
+#include "io.h"
+#include "door_state.h"
+
+#include <stdlib.h>
+
+static enum door_state{
+    door_closed,
+    door_open,
+    door_blocked
+
+};
+
+void door_init(){
+    door_state = door_open;
+    hardware_command_door_open(1);
+    if (hardware_read_obstruction_signal()){
+        door_state= door_blocked;
+    }
+    else{
+        door_state = door_closed;
+        hardware_command_door_open(0);
+    }  
+    
+}
+
+void open_door(){
+    if (lift_state_is_at_floor()){
+        hardware_command_door_open(1);
+    }
+    else{
+        print("cannot open doors. Lift is in undifined state");
+    }
+}
+void close_door(){
+    if (!hardware_read_obstruction_signal()){
+        hardware_command_door_open(0);
+    }
+    else{
+        print("cannot close doors. Door blocked");
+    }
+}
+
+door_state get_door_state(){
+    
+}
---
+++ skeleton_project/source/door_state.h
@@ -0,0 +1,12 @@
+#include "hardware.h"
+#include "channels.h"
+#include "io.h"
+
+#include <stdlib.h>
+
+enum door_state;
+
+void door_init();
+
+void open_door();
+void close_door();
